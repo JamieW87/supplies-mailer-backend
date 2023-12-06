45de8c3d107bad01c8f24fb181f3f41674d24197
JamieW87 <jamie@deblock.com>
2023-12-06 20:34:35 +0000

    setup main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,64 @@
-package server
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+
+	"one-stop/internal/config"
+	http2 "one-stop/internal/transport/http"
+)
+
+func main() {
+	log := logrus.New()
+	env, err := config.Get()
+	if err != nil {
+		fmt.Println("could not retrieve environment: ", err)
+		panic(err)
+	}
+	
+	r := gin.New()
+	http2.Register(r, log)
+	port := fmt.Sprintf(":%d", env.Port)
+
+	h := &http.Server{
+		Addr:    port,
+		Handler: r,
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	log.Info(fmt.Sprintf("Server running on : %d\n", env.Port))
+
+	go func() {
+		err := h.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Error("An error occurred", err)
+			log.Error("Terminating program", nil)
+			os.Exit(1)
+		}
+	}()
+	<-stop
+
+	log.Info("Shutting down server...")
+
+	timeout := time.Duration(5) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	err = h.Shutdown(ctx)
+	if err != nil {
+		log.Error("Failed to shutdown gracefully", err)
+	} else {
+		log.Info("Server shutdown gracefully")
+	}
+
+}
--- internal/transport/http/register.go
+++ internal/transport/http/register.go
@@ -1,1 +1,10 @@
 package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+func Register(r *gin.Engine, log *logrus.Logger) {
+
+}
